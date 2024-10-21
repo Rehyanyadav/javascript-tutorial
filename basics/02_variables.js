a92b66ca55d9689b783188fe969dd8c1aff5f3ab
rehyan_yadav <ritulyadav1984@gmail.com>
2024-10-21 15:34:34 +0530

    added the files variables and conversion

---
+++ basics/02_variables.js
@@ -0,0 +1,22 @@
+"use strict"; //* treated all JS Code as newer version 
+// alert (" test")  //* showing error we are using node js  not browser 
+
+let name = "alpha"  //* String
+let age = 20 //* integer
+let isLoggedIn = false //* boolean
+let state = null ;
+
+
+//* number => 2 to power 53
+//* bigint => for trading stock market etc 
+//* string => " " , ' '
+//* boolean => true ,false 
+//* null  = >  it is value and datatype both 
+//* undefined =>  value is not assigned 
+//* symbol =>  unique 
+
+
+//* object 
+console.log( typeof age );
+console.log(typeof null);
+console.log(typeof undefined);
